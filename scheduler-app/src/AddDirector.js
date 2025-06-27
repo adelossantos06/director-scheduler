819f9965fb477059e2d14366155b3e1f183bffbc
Alyssa De Los Santos <adelossantos6@gmail.com>
2025-06-27 10:43:27 -0500

    adds backend server

---
+++ scheduler-app/src/AddDirector.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+function AddDirector() {
+    const [name, setName] = useState('')
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default AddDirector;
--- scheduler-app/src/App.js
+++ scheduler-app/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [showForm, setShowForm] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>KENS5 DIRECTOR SCHEDULER</h1>
     </div>
   );
 }
